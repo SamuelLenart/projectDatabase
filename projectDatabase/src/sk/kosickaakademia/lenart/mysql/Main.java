827714bf32c2bda0a988cd0eb73e862d2c3be4ae
SamuelLenart <72248460+SamuelLenart@users.noreply.github.com>
2021-02-09 20:35:46 +0100

    Update Main.java

--- projectDatabase/src/sk/kosickaakademia/lenart/mysql/Main.java
+++ projectDatabase/src/sk/kosickaakademia/lenart/mysql/Main.java
@@ -1,17 +1,23 @@
 package sk.kosickaakademia.lenart.mysql;
 
 
-import sk.kosickaakademia.lenart.mysql.Helper.Help;
 import sk.kosickaakademia.lenart.mysql.entity.CapitalCity;
 import sk.kosickaakademia.lenart.mysql.entity.City;
 import sk.kosickaakademia.lenart.mysql.entity.Country;
 import sk.kosickaakademia.lenart.mysql.entity.Monument;
+import sk.kosickaakademia.lenart.mysql.json.Server;
 import sk.kosickaakademia.lenart.mysql.output.Output;
 
 import java.util.List;
 
 public class Main {
+
+    private static String url;
+    private static String username;
+    private static String password;
+
     public static void main(String[] args) {
+        
         Database database = new Database();
         Output out = new Output();
         Country country = database.getCountryInfo("Japan");
@@ -27,7 +33,12 @@ public class Main {
         List<CapitalCity> capitalCityList = database.getCapitalCities("Europe");
         database.printCapitalCities(capitalCityList);
 
-        List<Monument> monuments = Help.getMonuments();
+        List<Monument> monuments = Database.getMonuments();
         out.printMonuments(monuments);
+
+        Server server = new Server();
+        String json = "{\"country\":\"Japan\",\"city\":\"Tokyo\",\"monument\":\"Tokyo Tower\"}";
+        System.out.println(server.insertNewMonument(json, url, username, password));
     }
+
 }
